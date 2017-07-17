92c20a6976d693c7bb1923ee3472417efb456ef9
Derek <xuderekq@hotmail.com>
2017-07-17 12:23:03 +0800

    First build

---
+++ path---music-586c7e095e534a3909ec.js
@@ -0,0 +1,2 @@
+webpackJsonp([3],{"./node_modules/json-loader/index.js!./.cache/json/music.json":function(o,n){o.exports={pathContext:{}}}});
+//# sourceMappingURL=path---music-586c7e095e534a3909ec.js.map
