d89a8977fe7254b4aea68ac0bfba8f19d3328c6e
Derek Xu <xuderekq@hotmail.com>
2018-06-18 18:29:13 -0400

    Redesigned about page

--- path---404-586c7e095e534a3909ec.js
+++ path---404-586c7e095e534a3909ec.js
@@ -1,2 +1,2 @@
-webpackJsonp([10],{"./node_modules/json-loader/index.js!./.cache/json/404.json":function(o,n){o.exports={pathContext:{}}}});
+webpackJsonp([9],{"./node_modules/json-loader/index.js!./.cache/json/404.json":function(o,n){o.exports={pathContext:{}}}});
 //# sourceMappingURL=path---404-586c7e095e534a3909ec.js.map
